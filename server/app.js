28dd63ea7fb0f4827114fee3448d0a56b851a5cf
Mikko <mikko.vallaskivi@gmail.com>
2018-08-22 13:26:05 +0300

    mongodb connection and mongoose initiated

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const graphqlHTTP = require("express-graphql")
 const schema = require("./schema/schema")
+const mongoose = require("mongoose")
 
 const app = express()
+
+mongoose.connect('mongodb://mikko:testi123@ds125392.mlab.com:25392/mikko_bookinfo_db', { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+    console.log('connected to db!')
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
