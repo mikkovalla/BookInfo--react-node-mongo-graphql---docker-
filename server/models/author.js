c440ec37c6f114090eefaa9cf396f6ebb2bca808
Mikko <mikko.vallaskivi@gmail.com>
2018-08-29 14:03:44 +0300

    mongoose author validation

--- server/models/author.js
+++ server/models/author.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const authorSchema = new Schema({
-    name: String,
-    age: Number,
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        min: [16, 'that young really???'],
+        max: 99
+    }
 })
 
 module.exports = mongoose.model('Author', authorSchema)
