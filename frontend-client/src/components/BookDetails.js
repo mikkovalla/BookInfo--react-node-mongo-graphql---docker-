d5144259baafcb85e97adeb9bf0752ebe556c763
Mikko <mikko.vallaskivi@gmail.com>
2018-09-04 14:53:56 +0300

    bookdetails listing

--- frontend-client/src/components/BookDetails.js
+++ frontend-client/src/components/BookDetails.js
@@ -4,11 +4,34 @@ import { getBookQuery } from '../queries/queries'
 
 class BookDetails extends Component {
 
+    displayBookDetails(){
+        const {book} = this.props.data
+        if(book){
+            return(
+                <div>
+                    <h2>{book.name}</h2>
+                    <p>{book.genre}</p>
+                    <p>{book.author.name}</p>
+                    <p>All books by this author:</p>
+                    <ul className="other-books">
+                      { book.author.books.map(b =>{
+                          return <li key={b.id}>{b.name}</li>
+                      })}
+                    </ul>
+                </div>
+            )
+        } else {
+            return(
+                <div>No books selected!</div>
+            )
+        }
+    }
+
   render() {
       //console.log(this.props)
     return (
       <div id="details">
-          <p>Kirjan tiedot tulee tähän</p>
+          {this.displayBookDetails()}
       </div>
     )
   }
